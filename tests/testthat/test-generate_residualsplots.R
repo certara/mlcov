483c9f521458dbce2c706adad5272f9d327083bd
certara-mtalley <150705449+certara-mtalley@users.noreply.github.com>
2023-11-29 14:54:39 -0800

    Added tests
    Added tests

---
+++ tests/testthat/test-generate_residualsplots.R
@@ -0,0 +1,49 @@
+# Read in tab2 dataset
+data <- read.table(system.file(package = "mlcov", "supplementary", "tab2"), skip = 1, header = T)
+
+# Search and select covariates. This function can take a few minutes to run
+result <- suppressWarnings(MLCovSearch(data, #NONMEM output
+                      list_pop_param = c("V1","CL"),
+                      cov_continuous = c("AGE","WT","HT","BMI","ALB","CRT",
+                                         "FER","CHOL","WBC","LYPCT","RBC",
+                                         "HGB","HCT","PLT"),
+                      cov_factors = c("SEX","RACE","DIAB","ALQ","WACT","SMQ")))
+
+# Generate plots
+plot_CL <- suppressWarnings(generate_residualsplots(data,
+                                   list_pop_param = c("V1","CL"),
+                                   cov_continuous = c("AGE","WT","HT","BMI","ALB","CRT",
+                                                      "FER","CHOL","WBC","LYPCT","RBC",
+                                                      "HGB","HCT","PLT"),
+                                   cov_factors = c("SEX","RACE","DIAB","ALQ","WACT","SMQ"),
+                                   result_ML = result$result_ML, 
+                                   result_5folds = result$result_5folds, 
+                                   i=c('CL')))
+
+plot_V1 <- suppressWarnings(generate_residualsplots(data,
+                                list_pop_param = c("V1","CL"),
+                                cov_continuous = c("AGE","WT","HT","BMI","ALB","CRT",
+                                                   "FER","CHOL","WBC","LYPCT","RBC",
+                                                   "HGB","HCT","PLT"),
+                                cov_factors = c("SEX","RACE","DIAB","ALQ","WACT","SMQ"),
+                                result_ML = result$result_ML, 
+                                result_5folds = result$result_5folds, 
+                                i=c('V1')))
+
+testthat::test_that("generate_residualsplots does not return plots for Cl", {
+  testthat::expect_null(plot_CL)
+})
+
+testthat::test_that("generate_residualsplots returns a list of 3 plots for V1", {
+  
+  testthat::expect_true(is.list(plot_V1))
+  testthat::expect_length(plot_V1, 3)
+})
+
+testthat::test_that("test-generate_residualsplots returns the same plots each time for V1", {
+  
+  vdiffr::expect_doppelganger("HT Residuals", plot_V1$HT)
+  vdiffr::expect_doppelganger("PLT Residuals", plot_V1$PLT)
+  vdiffr::expect_doppelganger("SEX Residuals", plot_V1$SEX)
+})
+
---
+++ tests/testthat/test-mlcovsearch.R
@@ -0,0 +1,28 @@
+# Read in tab2 dataset
+data <- read.table(system.file(package = "mlcov", "supplementary", "tab2"), skip = 1, header = T)
+
+# Search and select covariates. This function can take a few minutes to run
+result <- suppressWarnings(MLCovSearch(data, #NONMEM output
+                      list_pop_param = c("V1","CL"),
+                      cov_continuous = c("AGE","WT","HT","BMI","ALB","CRT",
+                                         "FER","CHOL","WBC","LYPCT","RBC",
+                                         "HGB","HCT","PLT"),
+                      cov_factors = c("SEX","RACE","DIAB","ALQ","WACT","SMQ")))
+
+testthat::test_that("MLCovSearch result_ML is a data.frame with only one covariate selection", {
+  
+  testthat::expect_true(is.data.frame(result$result_ML))
+  
+  # Currently the function returns a row of NA in the demo. This count excludes that
+  testthat::expect_true(sum(!is.na(result$result_ML$cov_selected)) == 1)
+})
+
+testthat::test_that("MLCovSearch result_5folds is a data.frame with 5 non-NA covariates selected", {
+  
+  testthat::expect_true(is.data.frame(result$result_5folds))
+  testthat::expect_true(sum(!is.na(result$result_5folds[1,])) == 5)
+})
+
+testthat::test_that("MLCovSearch shap plots do not change", {
+  vdiffr::expect_doppelganger("shap plots", result$shap_plots)
+})
