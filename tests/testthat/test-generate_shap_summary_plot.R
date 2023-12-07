ee81b17eb0d75943c6dbec8d7269fdd430558070
certara-mtalley <150705449+certara-mtalley@users.noreply.github.com>
2023-12-06 19:52:15 -0800

    Updated tests
    Had some errors that weren't caught until by normal devtools::test

--- tests/testthat/test-generate_shap_summary_plot.R
+++ tests/testthat/test-generate_shap_summary_plot.R
@@ -28,8 +28,7 @@ result <- suppressWarnings(MLCovSearch(data, #NONMEM output
 testthat::test_that("generate_shap_plots default plots do not change", {
   testthat::skip_if_not(get_os() == "windows")
   
-  suppressWarnings(test.plots.default <-
-                     generate_shap_summary_plot(result))
+  test.plots.default <- suppressWarnings(generate_shap_summary_plot(result))
   
   vdiffr::expect_doppelganger("default shap plots", test.plots.default)
 })
@@ -37,22 +36,23 @@ testthat::test_that("generate_shap_plots default plots do not change", {
 testthat::test_that("generate_shap_plots custom plots do not change", {
   testthat::skip_if_not(get_os() == "windows")
   
-  suppressWarnings(
-    test.plots.custom <- generate_shap_summary_plot(
-      result,
-      x_bound = NULL,
-      dilute = TRUE,
-      scientific = TRUE,
-      my_format = NULL,
-      min_color_bound = "#336699",
-      max_color_bound = "#CC0066",
-      kind = "sina",
-      title = "Test Title",
-      title.position = 0.5,
-      ylab = "Test y-axis",
-      xlab = "Test x-axis"
+  test.plots.custom <-
+    suppressWarnings(
+      generate_shap_summary_plot(
+        result,
+        x_bound = NULL,
+        dilute = TRUE,
+        scientific = TRUE,
+        my_format = NULL,
+        min_color_bound = "#336699",
+        max_color_bound = "#CC0066",
+        kind = "sina",
+        title = "Test Title",
+        title.position = 0.5,
+        ylab = "Test y-axis",
+        xlab = "Test x-axis"
+      )
     )
-  )
   
-  vdiffr::expect_doppelganger("custom shap plots", test.plots.default)
+  vdiffr::expect_doppelganger("custom shap plots", test.plots.custom)
 })
