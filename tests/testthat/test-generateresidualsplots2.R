d41623781491e2849c504d39c1bc5536fa76d2d3
certara-mtalley <150705449+certara-mtalley@users.noreply.github.com>
2023-12-07 14:19:29 -0800

    Added generate_residualsplots2 function
    Incorporated new residual plots code into its own functions.

---
+++ tests/testthat/test-generateresidualsplots2.R
@@ -0,0 +1,44 @@
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
+
+# Read in tab2 dataset
+data <- read.table(system.file(package = "mlcov", "supplementary", "tab2"), skip = 1, header = T)
+
+# Search and select covariates. This function can take a few minutes to run
+result <- suppressWarnings(MLCovSearch(data, #NONMEM output
+                                       list_pop_param = c("V1","CL"),
+                                       cov_continuous = c("AGE","WT","HT","BMI","ALB","CRT",
+                                                          "FER","CHOL","WBC","LYPCT","RBC",
+                                                          "HGB","HCT","PLT"),
+                                       cov_factors = c("SEX","RACE","DIAB","ALQ","WACT","SMQ")))
+
+# Generate plots
+plot_CL <- suppressWarnings(generate_residualsplots2(data,
+                                                     result,
+                                                     i = c('CL')))
+
+plot_V1 <- suppressWarnings(generate_residualsplots2(data,
+                                                     result,
+                                                     i = c('V1')))
+
+testthat::test_that("generate_residualsplots does not return plots for Cl", {
+  testthat::expect_null(plot_CL)
+})
+
+testthat::test_that("generate_residualsplots does not return plots for V1", {
+  testthat::expect_null(plot_V1)
+})
